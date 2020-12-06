dfa49c1e189ba32dd8fb8a8269281fc411cb9367
99003186 <75474400+99003186@users.noreply.github.com>
2020-12-06 19:09:49 +0530

    Update customer-billing-system.h

--- 3_implementation/INC/customer-billing-system.h
+++ 3_implementation/INC/customer-billing-system.h
@@ -1,1 +1,50 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CUSTOMER-BILLING-SYSTEM_OPERATIONS_H__
+#define __CUSTOMER-BILLING-SYSTEM_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 + operand2
+*/
+ void input();
+ void writefile();
+ void search();
+ void output();
+
+/**
+*  subtracts the operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 - operand2
+*/
+
+
+/**
+*  multiply the operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 * operand2
+*/
+
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
 
